9a0da1029dab321fd6acf4e2be673f923bbd9647
Aejjee <mrpapeerclip21@gmail.com>
2024-08-16 16:48:06 -0600

    commit

---
+++ Source/GMTK2024/PartBaseComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "PartBaseComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GMTK2024_API UPartBaseComponent : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+};
