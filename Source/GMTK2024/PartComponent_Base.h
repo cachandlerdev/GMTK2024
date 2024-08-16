54dbee2e3ea30ba39630ee8f2eaee03b64c29c95
BagelSeasoning234 <70250943+cachandlerdev@users.noreply.github.com>
2024-08-16 16:00:40 -0700

    feat: create part component

---
+++ Source/GMTK2024/PartComponent_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "PartComponent_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GMTK2024_API UPartComponent_Base : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+};
